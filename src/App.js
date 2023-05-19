fa8f4a7368b0a13a8ed0f16fef6ddc6b355bb2d4
Manoj Mishra <manojmishra.deb@gmail.com>
2023-05-19 13:39:33 +0530

    Add auth with firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from './routes/navigation';
 import Home from './routes/home';
 import { Routes, Route } from 'react-router-dom';
 import Shop from './routes/shop';
+import SignIn from './routes/sign-in';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAINE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export { auth, signInWithGooglePopup };
