d11c13ea8d0067b3f1c53b31fa7b07661f2179a7
Manoj Mishra <manojmishra.deb@gmail.com>
2023-06-12 10:03:58 +0530

    Add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
 import { compose, legacy_createStore as createStore, applyMiddleware  } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-export default createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(
+  persistedReducer,
   undefined,
   compose(applyMiddleware(...middlewares))
 );
+
+export const persistor = persistStore(store)
